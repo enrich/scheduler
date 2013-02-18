4d37375b3036d2960f20ef7f518baf9e3f7223ec
joel <joel@truher.org>
2013-02-17 21:50:18 -0800

    different annealing method
    annealing the schedule itself produces mostly impossible
    transitions, so instead, i anneal the list-ordering consumed
    by the greedy scheduler.  transitions are list-item-swaps.
    
    the annealed ordering produces ~1.5% better objective
    function results than roi ordering, which doesn't seem
    like much.
    
    it's also really slow, thousands of iterations required.

--- src/sched.js
+++ src/sched.js
@@ -2,6 +2,7 @@
 
 // TODO: webworker
 var best = 0;
+/*
 _.each(_.range(2),function() {
   sched.current_schedule = sched.shuffle();
   sched.anneal(sched.current_schedule);
@@ -15,8 +16,10 @@ sched.render('1', sched.current_schedule);
 sched.renderseries(sched.cost, 'cost1', sched.current_schedule);
 sched.renderseries(sched.value, 'val1', sched.current_schedule);
 d3.select('div#viz1').append('h3').text('obj: ' + best);
+*/
 
 best = 0;
+/*
 _.each(_.range(2),function() {
   sched.current_schedule = sched.byValue();
   sched.anneal(sched.current_schedule);
@@ -30,19 +33,25 @@ sched.render('2', sched.current_schedule);
 sched.renderseries(sched.cost, 'cost2', sched.current_schedule);
 sched.renderseries(sched.value, 'val2', sched.current_schedule);
 d3.select('div#viz2').append('h3').text('obj: ' + best);
+*/
+
+// populate the schedule in the given order,
+// using the "allowed" function.
+var populateGreedy = function(sorted) {
+  var sched_current_schedule = sched.none();
+  _.each(sorted, function(s) {
+    var t = 0;
+    while (!sched.allowed(s, t, sched_current_schedule)) {
+      t++;
+    }
+    sched_current_schedule[s.i] = t;
+  });
+  return sched_current_schedule;
+};
 
 // no annealing
 var greedy = function(srt) {
-  // start with nothing
-  sched.current_schedule = sched.none();
-  var sorted = _.sortBy(sched.projects, srt.sorter);
-  _.each(sorted, function(s) {
-    var t = 0;
-    while (!sched.allowed(s, t, sched.current_schedule)) {
-      t++;
-    }
-    sched.current_schedule[s.i] = t;
-  });
+  sched.current_schedule = populateGreedy(srt.sorted);
   var obj = sched.objective(sched.current_schedule);
   console.log('greedy ' + srt.label + ' ' + obj);
   sched.render(srt.idx, sched.current_schedule);
@@ -50,13 +59,65 @@ var greedy = function(srt) {
   sched.renderseries(sched.value, 'val' + srt.idx, sched.current_schedule);
   d3.select('div#viz' + srt.idx).append('h3').text(srt.label + ' obj: ' + obj);
 };
+
+/*
 greedy({
-  sorter: function(x){return 0 - x.value_per_period;},
+  sorted: _.sortBy(sched.projects, 
+    function(x){return 0 - x.value_per_period;}
+  ),
   label: 'by value',
   idx: 3
 });
+*/
+
+// produces 638.
 greedy({
-  sorter: function(x){return 0 - (x.value_per_period/x.cost_per_period);},
+  sorted: _.sortBy(sched.projects,
+    function(x){return 0 - (x.value_per_period/x.cost_per_period);}
+  ),
   label: 'by roi',
   idx: 4
 });
+
+// anneal the sorted list.  start with the ROI list:
+
+var newlist = _.sortBy(sched.projects, function(x){return 0 - (x.value_per_period/x.cost_per_period);});
+var popsched = populateGreedy(newlist);
+var popobj = sched.objective(popsched);
+var temp = 3;
+
+// > 647 is attainable.
+_.each(_.range(5000), function(i) {
+  // in each iteration, swap two items
+  var s1 = _.random(newlist.length - 1);
+  var s2 = _.random(newlist.length - 1);
+  // make a copy of the schedule
+  var maybe = newlist.slice();
+  var t = maybe[s1];
+  maybe[s1] = maybe[s2];
+  maybe[s2] = t;
+  var mp = populateGreedy(maybe);
+  var mo = sched.objective(mp);
+  if (mo > popobj) {
+    popobj = mo;
+    newlist = maybe;
+    popsched = mp;
+    console.log('iter ' + i + ' t ' + temp + ' new better ' + popobj);
+  } else if (Math.abs(mo - popobj) < 0.0001) {
+    // no change
+  } else if (Math.exp((mo - popobj) / temp) > Math.random())  {
+    popobj = mo;
+    newlist = maybe;
+    popsched = mp;
+    console.log('iter ' + i + ' t ' + temp + ' allow worse ' + popobj);
+  } else {
+    //console.log('no change');
+  }
+  temp *= 0.999;
+});
+
+console.log('annealed greedy ' + popobj);
+sched.render(5, popsched);
+sched.renderseries(sched.cost, 'cost5', popsched);
+sched.renderseries(sched.value, 'val5', popsched);
+d3.select('div#viz5').append('h3').text('annealed greedy obj: ' + popobj);
